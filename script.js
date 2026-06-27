4c638b3dfe569167e456a54b3e8343ab9dd37b73
maibalochlls2011 <maibalochlls2011@gmail.com>
2026-06-27 14:03:40 +0700

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function showName() {
+
     let name = document.getElementById("name").value;
 
-    document.getElementById("result").innerHTML =
-        "Xin chào " + name + "!";
-}
+    if (name == "36") {
 
-function changeColor() {
-    document.getElementById("title").style.color = "red";
+        document.body.innerHTML = `
+        <div style="
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        height:100vh;
+        font-size:400px;
+        font-weight:bold;
+        color:red;
+        background:black;">
+        36
+        </div>
+        `;
+
+        return;
+    }
+
+    document.getElementById("result").innerHTML = "Xin chào " + name + "!";
 }
