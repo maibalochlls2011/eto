e99f63ab3075ca537e64d8c886bdf48433deef3b
maibalochlls2011 <maibalochlls2011@gmail.com>
2026-06-27 14:07:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ function showName() {
 
     let name = document.getElementById("name").value;
 
-    if (name == "36") {
+    // Nếu nhập 36
+    if (name === "36") {
 
         document.body.innerHTML = `
         <div style="
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        height:100vh;
-        font-size:400px;
-        font-weight:bold;
-        color:red;
-        background:black;">
-        36
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            width:100vw;
+            height:100vh;
+            background:black;
+            color:red;
+            font-size:40vw;
+            font-weight:bold;
+        ">
+            36
         </div>
         `;
 
         return;
     }
 
+    // Các trường hợp còn lại
     document.getElementById("result").innerHTML = "Xin chào " + name + "!";
 }
